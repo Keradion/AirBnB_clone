b9ea11c8af0306d79002e327922faf0d0e4fb225
Daniel Berihun <danielshitaye10@gmail.com>
2024-05-16 08:11:33 -0700

    user class

---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3 
+""" module holds definition of class User """ 
+
+class User:
+    """ Extends BaseModel class """
+    email: str = ''
+    password: str = ''
+    first_name: str = ''
+    last_name: str = ''
