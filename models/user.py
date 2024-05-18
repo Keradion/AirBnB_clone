616924fb7dec949e011c72f7446498aee0c75436
Daniel Berihun <danielshitaye10@gmail.com>
2024-05-18 09:04:01 -0700

    py style

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3 
-""" module holds definition of class User """ 
+#!/usr/bin/python3
+""" Defines class User """
 from models.base_model import BaseModel
+
 
 class User(BaseModel):
     """ Extends BaseModel class """
-    email = " "
-    password = " "
-    first_name = " "
-    last_name = " "
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
