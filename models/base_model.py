a30c7ebc447347592d23b6355b64634bcaa1793d
Daniel Berihun <danielshitaye10@gmail.com>
2024-05-18 06:08:35 -0700

    typo error

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,9 @@ class BaseModel:
                 elif key != '__class__':
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             # adding new instance to __objects
             models.storage.new(self)
 
